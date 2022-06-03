8813aa0dbe22d7454e59255f8522b3847703b939
Ricardo Ronchetti <89156389+ricardoronchetti@users.noreply.github.com>
2022-06-03 21:24:34 +0200

    Initial commit

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/jokes/random', (req, res) => {
+app.get('/events', (req, res) => {
   request(
-    { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+    { url: 'https://www.eventbrite.com/api/v3/destination/events/?event_ids=307641342537,270774021387,315008558077,272271008917,291036727687,290036144917,322769752017,330205933837,270774984267,194078352577,338233725177,291001161307,63168789550,319115492027,343831839287,350791716467,311471147587,314215616367,335641140687,65083832495&expand=event_sales_status,image,primary_venue,saves,ticket_availability,primary_organizer,public_collections&page_size=20' },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
         return res.status(500).json({ type: 'error', message: err.message });
